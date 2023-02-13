b063a0dbe52ee88f52d690fafc5e67ec9f8b344f
sachini1234 <sachinithilakshika1996@gmail.com>
2023-02-13 15:17:42 +0530

    my first commit

---
+++ src/Components/Education.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Education extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Education</h2>
+            </div>
+        )
+    }
+}
