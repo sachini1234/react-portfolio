911a186043e25fddc6bf851bcf91eaf1d7cdf68e
Sachini Thilakshika <sachinithilakshika1996@gmail.com>
2023-02-16 17:34:53 +0000

    Update Hobbies.js

---
+++ src/Components/Hobbies.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Hobbies extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hobbies</h1>
+                <p>This is Hobbies.</p>
+            </div>
+        )
+    }
+}
